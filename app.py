2faf879e5c3afc7a088464c01b1ee76f230cc89f
samjohn67 <sj6700191@gmail.com>
2025-05-24 04:54:31 -0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,34 @@
+# import samm
+from samm import balance as bal
+from samm import add_num
+
+balance = 40000
+# print(samm.my_name)
+# print(samm.age)
+# # print(samm.sentence)
+
+# print(balance)
+# print(bal)
+# print(add_num(6, 9))
+
+
+import random as ran
+guess = ran.randint(1,6)
+print(guess)
+
+guess_int = ran.randint(1,6)
+
+
+
+import random as ran
+
+cars = ["bmw", "ben",, "toyota", "volvo"]
+bias = [1,2,5,6]
+# sample = ran.sample(cars,k =2)
+# print(sample)
+# our_choice = ran.choice(cars)
+# print(choice) 
+ran.shuffle(cars)
+print(cars)
+
+
---
+++ samm.py
@@ -0,0 +1,9 @@
+# my_name = "samuel"
+
+# age = 70
+
+# sentence = "this is an external file from process file"
+# balance  = 3000000
+
+def add_num(x,y):
+    return x + Y
